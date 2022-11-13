bf99edabd741888eda0e4f2d5711c282082cf1eb
arupay <arupay11374@gmail.com>
2022-11-12 23:20:52 -0500

    categories component complete

--- components/Categories.js
+++ components/Categories.js
@@ -1,10 +1,58 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
+
+const items = [
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    text: "Pick-up",
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    text: "Bakery",
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("../assets/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../assets/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../assets/images/desserts.png"),
+    text: "Dessert",
+  },
+];
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
+    <View
+      style={{
+        marignTop: 5,
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+        paddingLeft: 20,
+      }}
+    >
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map((item, index) => (
+          <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+            <Image
+              source={item.image}
+              style={{ width: 50, height: 40, resizeMode: "contain" }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
