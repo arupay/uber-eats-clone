aad974a1d663ab2da87b89ae1138536385d6df27
arupay <arupay11374@gmail.com>
2022-11-12 23:52:15 -0500

    restaurant item component complete

---
+++ components/RestaurantItem.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+export default function RestaurantItem() {
+  return (
+    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 20 }}>
+      <View style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}>
+        <RestaurantImage />
+        <RestaurantInfo />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const RestaurantImage = () => (
+  <>
+    <Image
+      source={{
+        uri: "https://media-cdn.tripadvisor.com/media/photo-s/11/31/ee/3e/mancora-peruvian-restaurant.jpg",
+      }}
+      style={{ width: "100%", height: 180 }}
+    />
+    <TouchableOpacity style={{ position: "absolute", right: 20, top: 20 }}>
+      <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
+    </TouchableOpacity>
+  </>
+);
+
+const RestaurantInfo = () => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>Mancora Peru</Text>
+      <Text style={{ fontSize: 13, color: "gray" }}> 30-45 • min</Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: "gold",
+        height: 30,
+        width: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 15,
+      }}
+    >
+      <Text>4.5</Text>
+    </View>
+  </View>
+);
--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
+import RestaurantItem from "../components/RestaurantItem";
 import SearchBar from "../components/SearchBar";
 
 export default function Home() {
@@ -11,7 +12,10 @@ export default function Home() {
         <HeaderTabs />
         <SearchBar />
       </View>
-      <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItem />
+      </ScrollView>
     </SafeAreaView>
   );
 }
