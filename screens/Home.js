146e19821c78a55c52ea7beb3374474f0b040bcc
arupay <arupay11374@gmail.com>
2022-11-12 22:37:58 -0500

    search bar component complete, no api connect yet, installed vector images

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <HeaderTabs />
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
     </SafeAreaView>
   );
 }
