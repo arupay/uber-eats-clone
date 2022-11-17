f8ac075cde4dd5f41bf1dcf313809f8406d0be6b
arupay <arupay11374@gmail.com>
2022-11-17 17:56:05 -0500

    redux initialized w/ boiler plate code

--- navigation.js
+++ navigation.js
@@ -3,6 +3,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
 import RestaurantDetail from "./screens/RestaurantDetail";
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "./redux/store";
+
+const store = configureStore();
 
 export default function RootNavigation() {
   const Stack = createNativeStackNavigator();
@@ -10,11 +14,13 @@ export default function RootNavigation() {
     headerShown: false,
   };
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
   );
 }
